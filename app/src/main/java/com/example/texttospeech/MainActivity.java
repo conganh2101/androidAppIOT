55e640eaa3d7400fca10dd5d75dff37abe6fde89
conganh2101 <conganh2101@gmail.com>
2020-11-23 23:02:51 +0700

    demo 2

--- app/src/main/java/com/example/texttospeech/MainActivity.java
+++ app/src/main/java/com/example/texttospeech/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -13,18 +14,37 @@ import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class MainActivity extends Activity {
+
+    MqttAndroidClient client;
+    private ToggleButton relay;
+    private ToggleButton mainroom;
+    private ToggleButton bedroom;
     public static final Integer RecordAudioRequestCode = 1;
     private SpeechRecognizer speechRecognizer;
     private EditText editText;
@@ -34,6 +54,112 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        relay = findViewById(R.id.relay);
+        mainroom = findViewById(R.id.mainroom);
+        bedroom = findViewById(R.id.bedroom);
+
+        relay.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    publishMessage(1,1);
+                } else {
+                    publishMessage(1,0);
+                }
+            }
+        });
+
+        mainroom.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    publishMessage(2,1);
+                } else {
+                    publishMessage(2,0);
+                }
+            }
+        });
+
+        bedroom.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    publishMessage(3,1);
+                    Log.d("demo_control", "Led on");
+                } else {
+                    publishMessage(3,0);
+                    Log.d("demo_control", "Led on");
+                }
+            }
+        });
+        //Nếu broker yêu cầu mật khẩu
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setUserName("dxhoan");
+        options.setPassword("dxhoan".toCharArray());
+
+        String clientId = MqttClient.generateClientId();
+        client = new MqttAndroidClient(this.getApplicationContext(), "tcp://192.168.43.180:1883",
+                clientId);
+        client.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.d("mqtt", message.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+        try {
+            IMqttToken token = client.connect();
+            //Nếu sử dụng option IMqttToken token = client.connect(options);
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Log.d("mqtt", "onSuccess");
+//                    publishMessage("hello");
+//                    String topic = "foo/bar";
+//                    int qos = 1;
+//                    try {
+//                        IMqttToken subToken = client.subscribe(topic, qos);
+//                        subToken.setActionCallback(new IMqttActionListener() {
+//                            @Override
+//                            public void onSuccess(IMqttToken asyncActionToken) {
+//                                Log.d("mqtt", "onSuccess: subscribe");
+//                            }
+//
+//                            @Override
+//                            public void onFailure(IMqttToken asyncActionToken,
+//                                                  Throwable exception) {
+//                                // The subscription could not be performed, maybe the user was not
+//                                // authorized to subscribe on the specified topic e.g. using wildcards
+//
+//                            }
+//                        });
+//                    } catch (MqttException e) {
+//                        e.printStackTrace();
+//                    }
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.d("mqtt", "onFailure");
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
         if(ContextCompat.checkSelfPermission(this,Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
             checkPermission();
         }
@@ -131,6 +257,19 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == RecordAudioRequestCode && grantResults.length > 0 ){
             if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void publishMessage(int component, int status) {
+        String topic = "/home/control";
+        String payload = "{\"id\":" + String.valueOf(component) + ",\"vol\":" + String.valueOf(status) + "}";
+        byte[] encodedPayload = new byte[0];
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
+            client.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
         }
     }
 }
